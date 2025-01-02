129d5d6b45e035636886a008d5da22b68190ec1d
Raj Kapadia <37063055+RajKKapadia@users.noreply.github.com>
2025-01-02 11:54:16 +0530

    changed route name as filename

--- src/views/widget_view.py
+++ src/views/widget_view.py
@@ -3,6 +3,6 @@ from flask import Blueprint, send_file
 widget_view = Blueprint('widget_view', __name__, url_prefix="/api/widget")
 
 
-@widget_view.get("/")
+@widget_view.get("/chatWidget.js")
 def handle_get():
     return send_file('static/chatWidget.js', mimetype='application/javascript')
