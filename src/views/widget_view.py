8c421ba8b929c3e2a6ee6283e2888147aed4435f
RajKKapadia <raajforyou@gmail.com>
2025-01-04 21:58:50 +0530

    added code to get userId

--- src/views/widget_view.py
+++ src/views/widget_view.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, send_file
+from flask import Blueprint, send_file, request
+
+from src import logging
 
 widget_view = Blueprint('widget_view', __name__, url_prefix="/api/widget")
 
 
+logger = logging.getLogger(__name__)
+
+
 @widget_view.get("/chatWidget.js")
 def handle_get():
+    data = request.args.to_dict()
+    logger.info(data)
     return send_file('static/chatWidget.js', mimetype='application/javascript')
